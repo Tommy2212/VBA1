809f4bbd818fe6db41e64640a0f23baace2789e4
Tommy2212 <phambaotien91@gmail.com>
2020-07-24 10:48:43 +0700

    COMMIT THU 1 i

---
+++ aaa - Copy.bat
@@ -0,0 +1,2 @@
+"C:\Program Files (x86)\UiPath\Studio\UiRobot.exe" /file:"D:\RPA\LogisticsRPAcode\ASN - not GET\3-Dev\Code\SourceCode\Main.xaml" /input "{'str_IfAutoTrigger':'Y','str_StartPoint':'1.Initial Start Point','str_Factorys':'EAV','str_SelectExftyDate':'Tomorrow'}" 
+ 
---
+++ aaa.bat
@@ -0,0 +1,4 @@
+cd C:\Users\tienpb\AppData\Local\UiPath\app-19.11.0-beta0002 
+UiRobot.exe execute --file E:\RPA\test\BOOKING_NIKE.1.0.1.nupkg 
+input "{'str_Factorys':'EAV'}"
+pause
